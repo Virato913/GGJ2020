903cd437fd2931311e6e89976ab5eba1afb1d589
LAPTOP-BGAV0320\ramse <idv18c.rguerrero@uartesdigitales.edu.mx>
2020-02-01 11:02:25 -0600

    //Cosas inutiles

---
+++ Assets/Scripts/CFloor.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CFloor : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start(){
+    }
+    //TO DO: Función que va  a caer una bala y esa bala va a destruir un piso en especifico, lo marco con un circulo negro y un collider
+    // Update is called once per frame
+    void Update(){
+    }
+}
