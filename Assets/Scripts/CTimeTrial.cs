fc8ab27f1f8df391d282faca91eedd0618ab15bf
frankiALVarado <idv16c.falvarado@uartesdigitales.edu.mx>
2020-02-01 23:54:37 -0600

    Add Ctrial class as the one who puts the counter of the time left to survive

--- Assets/Scripts/CTimeTrial.cs
+++ Assets/Scripts/CTimeTrial.cs
@@ -1,21 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CTimeTrial : MonoBehaviour
 {
-    public float m_timer;
-    public float m_totaltime;
+    public float m_totaltimeleft;
+    public Text m_text;
     // Start is called before the first frame update
     void Start()
     {
-        m_timer = 0;
-        m_totaltime = 180;
+        m_totaltimeleft = 180;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
+        m_totaltimeleft -= Time.fixedDeltaTime;
+        if(m_totaltimeleft < 0)
+        {
+            m_totaltimeleft = 0;
+            //gameover();
+        }
+        m_text.text = Mathf.Floor(m_totaltimeleft).ToString();
         
     }
 }
